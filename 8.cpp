da333dc39aeba89c4a381a02e260d83e72311ef6
kshitij <kshitijgupta070704@gmail.com>
2024-04-25 14:33:02 +0530

    I am starting the sheet of c++ basic of 25 Ques from askSenior site

--- 8.cpp
+++ 8.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-#include <boost/multiprecision/cpp_int.hpp>
-namespace mp = boost::multiprecision;
-using BigInt = mp::cpp_int;
-
 int main()
 {
-    BigInt n;
+    long long n;
     cin >> n;
-    BigInt sum;
+    long long sum;
     if (n % 2 == 0)
     {
-        sum = (n * (n + 1)) / 2 - (2 * pow(n / 2, 2));
+        sum = n / 2;
     }
     else
     {
-        sum = (n * (n + 1)) / 2 - (2 * pow(n / 2 + 1, 2));
+        sum = -2 - (3 * n) / 2;
     }
     cout << sum;
 }
