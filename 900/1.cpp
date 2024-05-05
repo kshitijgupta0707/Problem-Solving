7116ea586d0c9eb7bd55404112fda9af08b3ab68
kshitij <kshitijgupta070704@gmail.com>
2024-05-05 22:45:31 +0530

    1904A - Forked! problem solved

--- 900/1.cpp
+++ 900/1.cpp
@@ -69,6 +69,7 @@ int main()
                 }
             }
         }
+  
         cout << count << endl;
     }
-}
+} 
--- 900/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-cout << " { " << p.first << " , " << p.second << " } " << endl;
-                    cout << " { " << q.first << " , " << q.second << " } " << endl;
