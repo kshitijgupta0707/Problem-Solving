54d088354849011f630c335f995ff2f47d790948
kshitij <kshitijgupta070704@gmail.com>
2024-05-18 16:17:35 +0530

    1794B - Not Dividing problem solved

---
+++ 900/10.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+// bits
+// test
+// inarr
+// outarr
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] == 1)
+            {
+                arr[i]++;
+                i = -1;
+                continue;
+            }
+            if (arr[i + 1] % arr[i] == 0)
+            {
+                arr[i + 1]++;
+                i = -1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+}
