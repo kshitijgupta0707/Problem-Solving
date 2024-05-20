7cb8c59d5d0cea210fd2e932064214e7a67b6c92
kshitij <kshitijgupta070704@gmail.com>
2024-05-20 20:56:00 +0530

    problem solved

---
+++ 800/35.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// bits
+// test
+// inarr
+// outarr
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        n++;
+        long long ans = n / 10;
+        cout << ans << endl;
+    }
+}
